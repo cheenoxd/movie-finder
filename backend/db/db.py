fb5eddfc40ce9ab6ce2fa872a05e1d1af3756d97
Zesan <zesanrahim@gmail.com>
2025-03-29 23:17:54 -0400

    feat: add mongoDB

--- backend/db/db.py
+++ backend/db/db.py
@@ -1,33 +1,16 @@
-
-import psycopg2
-from dotenv import load_dotenv
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-SSL_MODE = os.getenv("SSL_MODE")
+uri = os.getenv("MONGO_URI")
+
+client = MongoClient(uri, server_api=ServerApi('1'))
 
 try:
-    
-    connection = psycopg2.connect(
-        dbname=DB_NAME,
-        user=DB_USER,
-        password=DB_PASSWORD,
-        host=DB_HOST,
-        port=DB_PORT,
-        sslmode=SSL_MODE
-    )
-    print("Connection to the database established successfully!")
-
-    # Create a cursor for executing queries
-    cursor = connection.cursor()
-
-cursor.execute()
-connection.commit()
-cursor.close()
-connection.close()
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
